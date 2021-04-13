595d4bf72840e6f4f880e75678c892f687e55ae4
ducbang1510 <1851050006bang@ou.edu.vn>
2021-04-13 11:05:26 +0700

    delete

--- app.py
+++
@@ -1,5 +0,0 @@
-from app import create_app
-
-app = create_app('app.cfg')
-app.app_context().push()
-app = app.run()
--- app/store_pages/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-store_pages_blueprint = Blueprint('store_pages', __name__, template_folder='templates')
-
-from . import routes
