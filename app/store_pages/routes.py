bc85eae5e4d7915dd24f3ee9dd32cd76b5edc316
Văn Trung <trung.pv194@gmail.com>
2021-04-25 09:32:37 +0700

    comment code

--- app/store_pages/routes.py
+++ app/store_pages/routes.py
@@ -250,12 +250,12 @@ def checkout():
 
 
 # trung
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+#
+#
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # Nhập sách
